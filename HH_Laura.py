f39531c7d8874b6a99dd786eb6ec5c7e8b32e137
mnd498 <mnd498@alumni.ku.dk>
2023-03-22 13:44:54 +0100

    Laura

--- HH_Laura.py
+++ HH_Laura.py
@@ -1,11 +1,10 @@
-
 from types import SimpleNamespace
 import numpy as np
 from scipy import optimize
-import ipywidgets as widgets
 import pandas as pd 
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+
+
 
 class HouseholdSpecializationModelClass:
 
@@ -23,13 +22,13 @@ class HouseholdSpecializationModelClass:
         par.omega = 0.5 
 
         # c. household production
-        par.alpha = 0.25
-        par.sigma = 0.5
+        par.alpha = 0.5
+        par.sigma = 1
 
         # d. wages
         par.wM = 1.0
         par.wF = 1.0
-        par.wF_vec = np.linspace(0.8, 1.2, 5)
+        par.wF_vec = np.linspace(0.8,1.2,5)
 
         # e. targets
         par.beta0_target = 0.4
@@ -44,27 +43,31 @@ class HouseholdSpecializationModelClass:
         sol.beta0 = np.nan
         sol.beta1 = np.nan
 
+        #g. initial guess for allocation
+        par.guess = [7.1, 1.4, 1.5, 7.2]
+
     def calc_utility(self,LM,HM,LF,HF):
         """ calculate utility """
 
         par = self.par
-        sol = self.sol
+        sol = self.sol    
 
         # a. consumption of market goods
         C = par.wM*LM + par.wF*LF
 
         # b. home production
-        if par.sigma == 0:
-            H = min(HM, HF)
-        elif par.sigma == 1:
-            H = HM**(1-par.alpha)*HF**par.alpha 
-        else: 
-            power1=(par.sigma - 1)/par.sigma
-            power2=par.sigma/(par.sigma-1)
-            H = ((1 - par.alpha )*HM**(power1) + par.alpha * HF**(power1))**(power2)
+        if np.isclose(par.sigma,0):
+            H = min(HF, HM)
+        elif np.isclose(par.sigma,1): 
+            H = HM**(1-par.alpha)*HF**par.alpha
+        else:
+            power1 = (par.sigma-1)/par.sigma
+            power2 = par.sigma/(par.sigma-1)
+            H = ((1-par.alpha)*HM**(power1)+par.alpha*HF**(power1))**(power2)
+
 
         # c. total consumption utility
-        Q = (C**par.omega)*H**(1-par.omega)
+        Q = C**par.omega*H**(1-par.omega)
         utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
 
         # d. disutlity of work
@@ -140,8 +143,46 @@ class HouseholdSpecializationModelClass:
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
-
-        pass
+    
+        res = SimpleNamespace()
+        wF = self.par.wF_vec
+        it = len(wF)
+        Home_ratios = np.zeros(it)
+        LM = np.zeros(it)
+        HM = np.zeros(it)
+        LF = np.zeros(it)
+        HF = np.zeros(it)
+        if discrete == True:
+            for it, val in enumerate(wF): 
+                self.par.wF = val
+                opt = self.solve_discrete()
+                Home_ratios[it] = opt.HF / opt.HM ##Store the ratio
+                LM[it]= opt.LM #But also the individual values
+                HM[it]= opt.HM
+                LF[it]= opt.LF
+                HF[it]= opt.HF
+                res.Home_ratios = Home_ratios
+                res.LM = LM
+                res.HM = HM
+                res.LF = LF
+                res.HF = HF
+            return res
+        else: 
+            for it, val in enumerate(wF): 
+                self.par.wF = val
+                opt = self.solve_continuous()
+                Home_ratios[it] = opt.HF / opt.HM ##Store the ratio
+                LM[it]= opt.LM #But also the individual values
+                HM[it]= opt.HM
+                LF[it]= opt.LF
+                HF[it]= opt.HF
+                res.Home_ratios = Home_ratios
+                res.LM = LM
+                res.HM = HM
+                res.LF = LF
+                res.HF = HF
+            return res
+    
 
     def run_regression(self):
         """ run regression """
